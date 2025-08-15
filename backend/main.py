d3fb47034d90990c362f04136a4f69ae99707487
Russell <russelldcosta7@gmail.com>
2025-08-15 14:18:15 +0530

    working plain login and models setup

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+import models
 
 app = FastAPI()
+
+def get_db():                   # Dependency to get DB session
+    db = models.SessionLocal()
+    try:        yield db
+    finally:    db.close()
+
+@app.post("/faculty/login")
+def faculty_login(email: str, password: str, db: Session = Depends(get_db)):
+    faculty = db.query(models.Faculty).filter(models.Faculty.email == email).first()
+    if not faculty:                        raise HTTPException(status_code=404, detail="Invalid email")
+    if password != faculty.password_hash:  raise HTTPException(status_code=401, detail="Invalid password")      # plain password for now
+    return {"message": "Login successful"}
---
+++ backend/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# DB setup
+DATABASE_URL = "sqlite:///./database.db"        # uvicorn main:app --reload; creates tables
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Faculty(Base):
+    __tablename__ = "faculty"
+    id = Column(String, primary_key=True, index=True)  # e.g., "FAC-101"
+    first_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+class Class(Base):
+    __tablename__ = "classes"
+    id = Column(String, primary_key=True, index=True)  # e.g., "CS-402"
+    subject_name = Column(String, nullable=False)
+    faculty_id = Column(String, ForeignKey("faculty.id"), nullable=False)
+
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(String, primary_key=True, index=True)  # e.g., "22CSE1032"
+    name = Column(String, nullable=False)
+
+class Attendance(Base):
+    __tablename__ = "attendance"
+    id = Column(Integer, primary_key=True, index=True)
+    class_id = Column(String, ForeignKey("classes.id"), nullable=False)
+    student_id = Column(String, ForeignKey("students.id"), nullable=False)
+    date = Column(Date, nullable=False)
+    present = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind=engine)
